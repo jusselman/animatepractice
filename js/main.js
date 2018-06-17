9f150ed3d81c3a7e01503b7c31fc67e59753cdbb
jusselman <usselmanjosh@gmail.com>
2018-06-16 23:34:16 -0700

    gauge the  pixels

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+var counter0 = counter0.style.borderRadius=this.value+'px';
+
+var counter1 = counter1.style.height=this.value+'px';
+
+var counter2 = counter2.style.width=this.value+'px';
+
+// var box1 = document.getElementById("box1");
+// box1.style.transition = "width 5s ease 0s";
+// box1.addEventListener ( "transitionend", function() {
+//
+//       alert("Transition Has Ended");
+//
+// })
